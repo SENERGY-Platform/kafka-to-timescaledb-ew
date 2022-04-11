f74e95080b25f8d8bb77fff85127cbe061e80bbd
y-du <42994541+y-du@users.noreply.github.com>
2022-04-11 16:28:24 +0200

    init

---
+++ ew/converter.py
@@ -0,0 +1,60 @@
+"""
+   Copyright 2022 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import datetime
+
+
+def to_float(val, *args):
+    return float(val)
+
+
+def to_int(val, *args):
+    return int(val)
+
+
+def to_bool(val, *args):
+    return bool(val)
+
+
+def to_str(val, *args):
+    return str(val)
+
+
+time_fmt = "%Y-%m-%d %H:%M:%S.%f"
+
+
+def to_timestamp(val, fmt: str):
+    if fmt:
+        if fmt == "unix":
+            time_obj = datetime.datetime.fromtimestamp(val)
+        else:
+            time_obj = datetime.datetime.strptime(val, fmt)
+        return time_obj.strftime(time_fmt)
+    else:
+        return val
+
+
+type_map = {
+    "TIMESTAMP": to_timestamp,
+    "bool": to_bool,
+    "real": to_float,
+    "double": to_float,
+    "smallint": to_int,
+    "integer": to_int,
+    "bigint": to_int,
+    "varchar": to_str,
+    "text": to_str
+}
